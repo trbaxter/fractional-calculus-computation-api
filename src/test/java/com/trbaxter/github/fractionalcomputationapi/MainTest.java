5f6512059880c793c55ce53b2fdeeec0d84d3dda
trbaxter <trb7074@gmail.com>
2024-06-05 12:03:02 -0500

    addition: basic smoke test for Main

---
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/MainTest.java
@@ -0,0 +1,14 @@
+package com.trbaxter.github.fractionalcomputationapi;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Smoke test for the Main application class. <br/>
+ * Performs a basic sanity check ensuring that main() is executed without exceptions.
+ */
+public class MainTest {
+    @Test
+    public void main() {
+        Main.main(new String[] {});
+    }
+}
