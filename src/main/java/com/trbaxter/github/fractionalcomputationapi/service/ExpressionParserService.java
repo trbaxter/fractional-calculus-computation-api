d9dd63314255d73eebf74fa7ec9bbd588575c841
trbaxter <trb7074@gmail.com>
2024-06-07 06:06:52 -0500

    feature: allow brackets in expression

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/service/ExpressionParserService.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/service/ExpressionParserService.java
@@ -11,42 +11,67 @@ public class ExpressionParserService {
     /**
      * Method responsible for parsing the math expression provided by <br/>
      * the user by breaking it up into a collection of individual terms.
-     * <p>
-     * Begins by looping through every character of the expression string.<br/>
-     * Characters are appended to a term until a plus or minus sign is found.<br/>
-     * </p>
-	 * <p>
-	 * Once the terms are collected, another loop begins to check if any term<br/>
-	 * starts with an arithmetic operator. If that occurs, the operator is truncated.
-	 * </p>
      */
 
     public List<String> parseExpression(String expression) {
+		expression = removeWhitespace(expression);
 
-		expression = expression.replaceAll("\\s+", "");
+		List<String> terms = extractTerms(expression);
 
+		return cleanTerms(terms);
+	}
+
+	// Extracts the individual terms from the provided expression
+	private List<String> extractTerms(String expression) {
 		List<String> terms = new ArrayList<>();
 		StringBuilder term = new StringBuilder();
+		int bracketLevel = 0;
 
 		for (int i = 0; i < expression.length(); i++) {
 			char c = expression.charAt(i);
 
-			if (c == '+' || c == '-') {
+			if (isOpeningBracket(c)) {
+				bracketLevel++;
+				term.append(c);
+			} else if (isClosingBracket(c)) {
+				bracketLevel--;
+				term.append(c);
+			} else if (bracketLevel == 0 && (c == '+' || c == '-')) {
 				terms.add(term.toString());
 				term = new StringBuilder();
+				term.append(c);
+			} else {
+				term.append(c);
 			}
-			else {term.append(c);}
 		}
 
 		terms.add(term.toString());
+		return terms;
+	}
 
+	// Removes all whitespaces from expression
+	private String removeWhitespace(String expression) {
+		return expression.replaceAll("\\s+", "");
+	}
+
+	// Cleans terms by removing leading arithmetic operators
+	private List<String> cleanTerms(List<String> terms) {
 		for (int i = 0; i < terms.size(); i++) {
 			String t = terms.get(i);
 			if (t.startsWith("+") || t.startsWith("-") || t.startsWith("*") || t.startsWith("/")) {
 				terms.set(i, t.substring(1));
 			}
 		}
-
 		return terms;
+	}
+
+	// Checks if the  character is an opening bracket
+	private boolean isOpeningBracket(char c) {
+		return c == '(' || c == '[';
+	}
+
+	// Checks if the inspected character is a closing bracket
+	private boolean isClosingBracket(char c) {
+		return c == ')' || c == ']';
 	}
 }
