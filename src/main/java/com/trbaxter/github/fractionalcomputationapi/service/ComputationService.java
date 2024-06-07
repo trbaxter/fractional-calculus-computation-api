c9cfaf0a437a64646bdfce001465d84c73800711
trbaxter <trb7074@gmail.com>
2024-06-07 08:19:40 -0500

    optimization: modularized Comp. Service

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/service/ComputationService.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/service/ComputationService.java
@@ -19,34 +19,42 @@ public class ComputationService {
 	private MathOperationsService mathOperationsService;
 
 	public String derivative(String expression, double order) {
+		validateInput(expression, order);
 
-		if (expression == null || expression.isEmpty()) {
-			throw new IllegalArgumentException(
-			"Input error - please review input expression and order value.");
-		}
-		else if (order < 0) {
-			throw new IllegalArgumentException(
-			"Operation order must be greater than equal to zero.");
-			// TODO: Expand this functionality in the future.
-		}
-		else if (order == 0) {
+		// Derivative of order 0 should just return the function back to user
+		if (order == 0) {
 			return expression;
 		}
 
+		// Parse through the expression and break into terms that can be evaluated
 		List<String> terms = expressionParserService.parseExpression(expression);
 
 		List<Double> coefficients = new ArrayList<>();
 		List<Character> variables = new ArrayList<>();
 		List<Double> exponents = new ArrayList<>();
 
+		// Add the coefficients, variables, and exponents from terms into the arraylists
 		termProcessingService.fillOutLists(terms, coefficients, variables, exponents);
 
 		List<Double> newCoefficients = new ArrayList<>();
 		List<Double> newExponents = new ArrayList<>();
 
-		mathOperationsService.performCalculations(coefficients, exponents, order, newCoefficients, newExponents);
+		// Perform the derivative operation and use it to fill out the new arraylists to create new terms
+        mathOperationsService.performCalculations(coefficients, exponents, order, newCoefficients, newExponents);
 
-        return mathOperationsService.reassembleExpandedExpression(newCoefficients, variables, newExponents);
+		// Reassemble all the new terms back into a single expression
+		return mathOperationsService.reassembleExpandedExpression(newCoefficients, variables, newExponents);
+	}
+
+	// Checks for empty/null expressions or negative orders that can't be handled yet
+	private void validateInput(String expression, double order) {
+		if (expression == null || expression.isEmpty()) {
+			throw new IllegalArgumentException("Input error - please review input expression and order value.");
+		}
+		else if (order < 0) {
+			throw new IllegalArgumentException("Operation order must be greater than equal to zero.");
+			// TODO: Expand this functionality in the future.
+		}
 	}
 
 //  TODO: Uncomment this when ready
