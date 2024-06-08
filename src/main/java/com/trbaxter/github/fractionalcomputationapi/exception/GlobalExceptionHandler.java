616903cebfb5f30ddc2a2e28f8a752202ec10f0b
trbaxter <trb7074@gmail.com>
2024-06-08 09:11:53 -0500

    glob. exception. update for validator

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/exception/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package com.trbaxter.github.fractionalcomputationapi.exception;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,5 +31,17 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<String> handleNumberFormatException(NumberFormatException e) {
 		return ResponseEntity.badRequest().body("Input not recognized.");
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return ResponseEntity.badRequest().body(errors);
 	}
 }
