0f61996ff1a7cd99e281f59b838db591367a678d
trbaxter <trb7074@gmail.com>
2024-06-14 19:00:02 -0500

    cleanup: reformat code

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/model/Term.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/model/Term.java
@@ -4,8 +4,8 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public record Term(BigDecimal coefficient, BigDecimal power) {
-	public Term {
-		Objects.requireNonNull(coefficient, "Coefficient must not be null");
-		Objects.requireNonNull(power, "Power must not be null");
-	}
+  public Term {
+    Objects.requireNonNull(coefficient, "Coefficient must not be null");
+    Objects.requireNonNull(power, "Power must not be null");
+  }
 }
