2eab11c6f8cffbea651d179ad0f7242c914a024c
trbaxter <trb7074@gmail.com>
2024-06-14 19:27:03 -0500

    cleanup: reformat code

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/controller/IndexController.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/controller/IndexController.java
@@ -2,7 +2,8 @@ package com.trbaxter.github.fractionalcomputationapi.controller;
 
 import com.trbaxter.github.fractionalcomputationapi.model.ControllerRequest;
 import com.trbaxter.github.fractionalcomputationapi.service.derivation.caputo.CaputoDerivativeService;
-import com.trbaxter.github.fractionalcomputationapi.service.derivation.RiemannLiouvilleDerivativeService;
+import com.trbaxter.github.fractionalcomputationapi.service.derivation.riemann_liouville.RiemannLiouvilleDerivativeService;
+import com.trbaxter.github.fractionalcomputationapi.service.integration.caputo.CaputoIntegrationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,38 +15,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("fractional-calculus-computation-api/derivative")
+@RequestMapping("fractional-calculus-computation-api/")
 @Validated
 public class IndexController {
 
-	private final CaputoDerivativeService caputoDerivativeService;
-	private final RiemannLiouvilleDerivativeService riemannLiouvilleDerivativeService;
+  private final CaputoDerivativeService caputoDerivativeService;
+  private final CaputoIntegrationService caputoIntegrationService;
+  private final RiemannLiouvilleDerivativeService riemannLiouvilleDerivativeService;
 
-	@Autowired
-	public IndexController(CaputoDerivativeService caputoDerivativeService,
-			RiemannLiouvilleDerivativeService riemannLiouvilleDerivativeService) {
-		this.caputoDerivativeService = caputoDerivativeService;
-		this.riemannLiouvilleDerivativeService = riemannLiouvilleDerivativeService;
-	}
+  @Autowired
+  public IndexController(
+      CaputoDerivativeService caputoDerivativeService,
+      RiemannLiouvilleDerivativeService riemannLiouvilleDerivativeService,
+      CaputoIntegrationService caputoIntegrationService) {
+    this.caputoDerivativeService = caputoDerivativeService;
+    this.riemannLiouvilleDerivativeService = riemannLiouvilleDerivativeService;
+    this.caputoIntegrationService = caputoIntegrationService;
+  }
 
-	@PostMapping("/caputo")
-	public ResponseEntity<String> computeCaputoDerivative(@Valid @RequestBody ControllerRequest request) {
-		try {
-			String result = caputoDerivativeService.evaluateExpression(request.getCoefficients(), request.getOrder());
-			return new ResponseEntity<>(result, HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @PostMapping("derivative/caputo")
+  public ResponseEntity<String> computeCaputoDerivative(
+      @Valid @RequestBody ControllerRequest request) {
+    try {
+      String result =
+          caputoDerivativeService.evaluateExpression(request.getCoefficients(), request.getOrder());
+      return new ResponseEntity<>(result, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 
-	@PostMapping("/riemann-liouville")
-	public ResponseEntity<String> computeRiemannLiouvilleDerivative(@Valid @RequestBody ControllerRequest request) {
-		try {
-			String result = riemannLiouvilleDerivativeService.evaluateExpression(request.getCoefficients(),
-					request.getOrder());
-			return new ResponseEntity<>(result, HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
+  @PostMapping("derivative/riemann-liouville")
+  public ResponseEntity<String> computeRiemannLiouvilleDerivative(
+      @Valid @RequestBody ControllerRequest request) {
+    try {
+      String result =
+          riemannLiouvilleDerivativeService.evaluateExpression(
+              request.getCoefficients(), request.getOrder());
+      return new ResponseEntity<>(result, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PostMapping("integral/caputo")
+  public ResponseEntity<String> computeCaputoIntegral(
+      @Valid @RequestBody ControllerRequest request) {
+    try {
+      String result =
+          caputoIntegrationService.evaluateExpression(
+              request.getCoefficients(), request.getOrder());
+      return new ResponseEntity<>(result, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
