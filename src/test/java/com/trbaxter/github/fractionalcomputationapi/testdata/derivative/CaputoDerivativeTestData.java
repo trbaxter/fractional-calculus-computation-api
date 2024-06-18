a8836a78fa8940c2e9b9d2d0bb2f0ec6d7215dc9
trbaxter <trb7074@gmail.com>
2024-06-17 20:21:57 -0500

    add'n: final access modifier added

--- src/test/java/com/trbaxter/github/fractionalcomputationapi/testdata/derivative/CaputoDerivativeTestData.java
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/testdata/derivative/CaputoDerivativeTestData.java
@@ -1,9 +1,9 @@
-package com.trbaxter.github.fractionalcomputationapi.testdata;
+package com.trbaxter.github.fractionalcomputationapi.testdata.derivative;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
 
-public class CaputoDerivativeTestData {
+public final class CaputoDerivativeTestData {
 
   public static Stream<Arguments> coefficientCombinations() {
     return Stream.of(
