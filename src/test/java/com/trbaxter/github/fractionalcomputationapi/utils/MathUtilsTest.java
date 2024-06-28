3dc03c6f7946dab6c30df1074c7407fb5c643f0f
trbaxter <trb7074@gmail.com>
2024-06-28 12:04:12 -0500

    optimization: math utils

--- src/test/java/com/trbaxter/github/fractionalcomputationapi/utils/MathUtilsTest.java
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/utils/MathUtilsTest.java
@@ -1,10 +1,6 @@
 package com.trbaxter.github.fractionalcomputationapi.utils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -21,52 +17,44 @@ import org.junit.jupiter.api.function.Executable;
  */
 public class MathUtilsTest {
 
-  /** Tests the gamma function for positive integer values. */
+  private static final int SCALE = 15;
+
   @Test
   public void testGammaFunctionPositiveValues() {
-    BigDecimal input = new BigDecimal("5");
-    BigDecimal expected = new BigDecimal("24.000000000000000");
-    BigDecimal result = MathUtils.gamma(input).setScale(15, RoundingMode.HALF_UP);
-    assertEquals(expected, result, "Gamma function failed for positive values");
+    assertGammaFunction(
+        new BigDecimal("5"), new BigDecimal("24.000000000000000"), "positive values");
   }
 
-  /** Tests the gamma function for half values (e.g., 0.5). */
   @Test
   public void testGammaFunctionHalfValues() {
-    BigDecimal input = new BigDecimal("0.5");
-    BigDecimal expected = BigDecimal.valueOf(Math.sqrt(Math.PI)).setScale(15, RoundingMode.HALF_UP);
-    BigDecimal result = MathUtils.gamma(input).setScale(15, RoundingMode.HALF_UP);
-    assertEquals(expected, result, "Gamma function failed for half values");
+    BigDecimal expected =
+        BigDecimal.valueOf(Math.sqrt(Math.PI)).setScale(SCALE, RoundingMode.HALF_UP);
+    assertGammaFunction(new BigDecimal("0.5"), expected, "half values");
   }
 
-  /** Tests the gamma function for negative values. */
   @Test
   public void testGammaFunctionNegativeValues() {
-    BigDecimal input = new BigDecimal("-0.5");
-    BigDecimal expected = new BigDecimal("-3.5449077018110318").setScale(15, RoundingMode.HALF_UP);
-    BigDecimal result = MathUtils.gamma(input).setScale(15, RoundingMode.HALF_UP);
-    assertEquals(expected, result, "Gamma function failed for negative values");
+    assertGammaFunction(
+        new BigDecimal("-0.5"), new BigDecimal("-3.544907701811032"), "negative values");
   }
 
-  /** Tests the gamma function for values near zero. */
   @Test
   public void testGammaFunctionNearZero() {
-    BigDecimal input = new BigDecimal("0.01");
-    BigDecimal expected = new BigDecimal("99.432585119150600").setScale(15, RoundingMode.HALF_UP);
-    BigDecimal result = MathUtils.gamma(input).setScale(15, RoundingMode.HALF_UP);
-    assertEquals(expected, result, "Gamma function failed near zero");
+    assertGammaFunction(
+        new BigDecimal("0.01"), new BigDecimal("99.432585119150600"), "values near zero");
   }
 
-  /** Tests the gamma function for the special case of input value 1. */
   @Test
   public void testGammaFunctionSpecialCase() {
-    BigDecimal input = new BigDecimal("1");
-    BigDecimal expected = BigDecimal.ONE.setScale(15, RoundingMode.HALF_UP);
-    BigDecimal result = MathUtils.gamma(input).setScale(15, RoundingMode.HALF_UP);
-    assertEquals(expected, result, "Gamma function failed for special case");
+    assertGammaFunction(
+        BigDecimal.ONE, BigDecimal.ONE.setScale(SCALE, RoundingMode.HALF_UP), "special case");
   }
 
-  /** Tests that the constructor of the MathUtils class throws an UnsupportedOperationException. */
+  private void assertGammaFunction(BigDecimal input, BigDecimal expected, String message) {
+    BigDecimal result = MathUtils.gamma(input).setScale(SCALE, RoundingMode.HALF_UP);
+    assertEquals(expected, result, "Gamma function failed for " + message);
+  }
+
   @Test
   public void testMathUtilsConstructorThrowsException() {
     Constructor<MathUtils> constructor = null;
@@ -96,11 +84,9 @@ public class MathUtilsTest {
     }
   }
 
-  /** Tests the gamma function with invalid input. */
   @Test
   public void testGammaFunctionWithInvalidInput() {
     BigDecimal invalidInput = mock(BigDecimal.class);
-
     when(invalidInput.doubleValue()).thenThrow(new NumberFormatException("Invalid input"));
 
     IllegalArgumentException thrownException =
