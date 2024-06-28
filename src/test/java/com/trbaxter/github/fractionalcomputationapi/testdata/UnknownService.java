f40a1e33944f2734c68625ba5faa4e8ce5d80b6e
trbaxter <trb7074@gmail.com>
2024-06-28 08:49:54 -0500

    cleanup and add test: unknown service error

---
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/testdata/UnknownService.java
@@ -0,0 +1,7 @@
+package com.trbaxter.github.fractionalcomputationapi.testdata;
+
+public class UnknownService {
+  public String evaluateExpression(String polynomialExpression, double order, int precision) {
+    return "Unknown service result";
+  }
+}
