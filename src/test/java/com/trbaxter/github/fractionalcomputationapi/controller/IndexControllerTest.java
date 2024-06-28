35b20cae1727864192bb0d3cb9e7bb2e46389c11
trbaxter <trb7074@gmail.com>
2024-06-27 23:41:08 -0500

    add logging checks for IndexControllerTest

--- src/test/java/com/trbaxter/github/fractionalcomputationapi/controller/IndexControllerTest.java
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/controller/IndexControllerTest.java
@@ -12,6 +12,8 @@ import com.trbaxter.github.fractionalcomputationapi.service.integration.caputo.C
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -25,6 +27,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(IndexController.class)
 public class IndexControllerTest {
 
+  private static final Logger logger = LoggerFactory.getLogger(IndexControllerTest.class);
+
   @Autowired private MockMvc mockMvc;
 
   @MockBean private CaputoDerivativeService caputoDerivativeService;
@@ -35,29 +39,27 @@ public class IndexControllerTest {
 
   @Autowired private ObjectMapper objectMapper;
 
-  /** Sets up the test environment before each test. */
   @BeforeEach
   public void setUp() {
     MockitoAnnotations.openMocks(this);
   }
 
-  /**
-   * Tests the computeCaputoDerivative endpoint with valid input.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
-  @Test
-  public void testComputeCaputoDerivative() throws Exception {
-    String polynomialExpression = "3x^2 + 2x + 1";
-    double alpha = 0.5;
-    Integer precision = 3;
-
+  private ControllerRequest createRequest(String polynomial, double alpha, Integer precision) {
     ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(polynomialExpression);
+    request.setPolynomialExpression(polynomial);
     request.setOrder(alpha);
     request.setPrecision(precision);
+    return request;
+  }
 
-    when(caputoDerivativeService.evaluateExpression(polynomialExpression, alpha, precision))
+  @Test
+  public void testComputeCaputoDerivative() throws Exception {
+    String polynomial = "3x^2 + 2x + 1";
+    double alpha = 0.5;
+    Integer precision = 3;
+    ControllerRequest request = createRequest(polynomial, alpha, precision);
+
+    when(caputoDerivativeService.evaluateExpression(polynomial, alpha, precision))
         .thenReturn("4.514x^1.5 + 2.257x^0.5");
 
     mockMvc
@@ -67,26 +69,18 @@ public class IndexControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.expression").value("4.514x^1.5 + 2.257x^0.5"));
+
+    logger.info("testComputeCaputoDerivative passed");
   }
 
-  /**
-   * Tests the computeRiemannLiouvilleDerivative endpoint with valid input.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
   @Test
   public void testComputeRiemannLiouvilleDerivative() throws Exception {
-    String polynomialExpression = "3x^2 + 2x + 1";
+    String polynomial = "3x^2 + 2x + 1";
     double alpha = 0.5;
     Integer precision = 3;
+    ControllerRequest request = createRequest(polynomial, alpha, precision);
 
-    ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(polynomialExpression);
-    request.setOrder(alpha);
-    request.setPrecision(precision);
-
-    when(riemannLiouvilleDerivativeService.evaluateExpression(
-            polynomialExpression, alpha, precision))
+    when(riemannLiouvilleDerivativeService.evaluateExpression(polynomial, alpha, precision))
         .thenReturn("4.514x^1.5 + 2.257x^0.5 + 0.564x^-0.5");
 
     mockMvc
@@ -96,25 +90,18 @@ public class IndexControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.expression").value("4.514x^1.5 + 2.257x^0.5 + 0.564x^-0.5"));
+
+    logger.info("testComputeRiemannLiouvilleDerivative passed");
   }
 
-  /**
-   * Tests the computeCaputoIntegral endpoint with valid input.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
   @Test
   public void testComputeCaputoIntegral() throws Exception {
-    String polynomialExpression = "3x^2 + 2x + 1";
+    String polynomial = "3x^2 + 2x + 1";
     double alpha = 0.5;
     Integer precision = 3;
+    ControllerRequest request = createRequest(polynomial, alpha, precision);
 
-    ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(polynomialExpression);
-    request.setOrder(alpha);
-    request.setPrecision(precision);
-
-    when(caputoIntegrationService.evaluateExpression(polynomialExpression, alpha, precision))
+    when(caputoIntegrationService.evaluateExpression(polynomial, alpha, precision))
         .thenReturn("1.805x^2.5 + 1.505x^1.5 + 1.128x^0.5 + C");
 
     mockMvc
@@ -124,17 +111,14 @@ public class IndexControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$.expression").value("1.805x^2.5 + 1.505x^1.5 + 1.128x^0.5 + C"));
+
+    logger.info("testComputeCaputoIntegral passed");
   }
 
-  /**
-   * Tests the computeCaputoDerivative endpoint with invalid input.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
   @Test
   public void testInvalidControllerRequest() throws Exception {
     ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(null);
+    request.setPolynomialExpression(null); // This should trigger the validation error
     request.setOrder(0.5);
     request.setPrecision(3);
 
@@ -144,25 +128,18 @@ public class IndexControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isBadRequest());
+
+    logger.warn("testInvalidControllerRequest passed - handled bad request");
   }
 
-  /**
-   * Tests the computeCaputoDerivative endpoint when an internal server error occurs.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
   @Test
   public void testCaputoDerivativeInternalServerError() throws Exception {
-    String polynomialExpression = "3x^2 + 2x + 1";
+    String polynomial = "3x^2 + 2x + 1";
     double alpha = 0.5;
     Integer precision = 3;
+    ControllerRequest request = createRequest(polynomial, alpha, precision);
 
-    ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(polynomialExpression);
-    request.setOrder(alpha);
-    request.setPrecision(precision);
-
-    when(caputoDerivativeService.evaluateExpression(polynomialExpression, alpha, precision))
+    when(caputoDerivativeService.evaluateExpression(polynomial, alpha, precision))
         .thenThrow(new RuntimeException("Internal Server Error"));
 
     mockMvc
@@ -172,25 +149,18 @@ public class IndexControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isInternalServerError())
         .andExpect(content().json("{\"expression\": \"Internal Server Error\"}"));
+
+    logger.error("testCaputoDerivativeInternalServerError passed - handled internal server error");
   }
 
-  /**
-   * Tests the computeRiemannLiouvilleDerivative endpoint when an internal server error occurs.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
   @Test
   public void testRiemannLiouvilleDerivativeInternalServerError() throws Exception {
-    String polynomialExpression = "3x^2 + 2x + 1";
+    String polynomial = "3x^2 + 2x + 1";
     double alpha = 0.5;
     Integer precision = 3;
-    ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(polynomialExpression);
-    request.setOrder(alpha);
-    request.setPrecision(3);
+    ControllerRequest request = createRequest(polynomial, alpha, precision);
 
-    when(riemannLiouvilleDerivativeService.evaluateExpression(
-            polynomialExpression, alpha, precision))
+    when(riemannLiouvilleDerivativeService.evaluateExpression(polynomial, alpha, precision))
         .thenThrow(new RuntimeException("Internal Server Error"));
 
     mockMvc
@@ -200,24 +170,19 @@ public class IndexControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isInternalServerError())
         .andExpect(content().json("{\"expression\": \"Internal Server Error\"}"));
+
+    logger.error(
+        "testRiemannLiouvilleDerivativeInternalServerError passed - handled internal server error");
   }
 
-  /**
-   * Tests the computeCaputoIntegral endpoint when an internal server error occurs.
-   *
-   * @throws Exception if an error occurs during the test.
-   */
   @Test
   public void testCaputoIntegralInternalServerError() throws Exception {
-    String polynomialExpression = "3x^2 + 2x + 1";
+    String polynomial = "3x^2 + 2x + 1";
     double alpha = 0.5;
     Integer precision = 3;
-    ControllerRequest request = new ControllerRequest();
-    request.setPolynomialExpression(polynomialExpression);
-    request.setOrder(alpha);
-    request.setPrecision(3);
+    ControllerRequest request = createRequest(polynomial, alpha, precision);
 
-    when(caputoIntegrationService.evaluateExpression(polynomialExpression, alpha, precision))
+    when(caputoIntegrationService.evaluateExpression(polynomial, alpha, precision))
         .thenThrow(new RuntimeException("Internal Server Error"));
 
     mockMvc
@@ -227,5 +192,7 @@ public class IndexControllerTest {
                 .content(objectMapper.writeValueAsString(request)))
         .andExpect(status().isInternalServerError())
         .andExpect(content().json("{\"expression\": \"Internal Server Error\"}"));
+
+    logger.error("testCaputoIntegralInternalServerError passed - handled internal server error");
   }
 }
