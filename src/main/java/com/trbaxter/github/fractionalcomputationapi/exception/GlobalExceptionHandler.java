fa3c90b885d30a59ab313edf9b1842c58bf1e9ee
trbaxter <trb7074@gmail.com>
2024-06-29 19:37:57 -0500

    optimization and refactoring contents

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/exception/GlobalExceptionHandler.java
@@ -1,11 +1,11 @@
 package com.trbaxter.github.fractionalcomputationapi.exception;
 
-import com.trbaxter.github.fractionalcomputationapi.model.ComputationResponse;
+import com.trbaxter.github.fractionalcomputationapi.model.Result;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,24 +16,23 @@ public class GlobalExceptionHandler {
   private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<ComputationResponse> handleValidationExceptions(
-      MethodArgumentNotValidException ex) {
+  public ResponseEntity<Result> handleValidationExceptions(MethodArgumentNotValidException ex) {
     String errorMessage =
         ex.getBindingResult().getAllErrors().stream()
-            .map(error -> ((FieldError) error).getField() + ": " + error.getDefaultMessage())
+            .map(error -> Optional.ofNullable(error.getDefaultMessage()).orElse("Validation error"))
             .findFirst()
             .orElse("Validation error");
 
     logger.warn("Validation error: {}", errorMessage);
 
     return new ResponseEntity<>(
-        new ComputationResponse("Validation Error: " + errorMessage), HttpStatus.BAD_REQUEST);
+        new Result("Validation Error: " + errorMessage), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<ComputationResponse> handleException(Exception e) {
+  public ResponseEntity<Result> handleException(Exception e) {
     logger.error("Unhandled exception: ", e);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(new ComputationResponse("Internal Server Error: " + e.getMessage()));
+        .body(new Result("Internal Server Error: " + e.getMessage()));
   }
 }
