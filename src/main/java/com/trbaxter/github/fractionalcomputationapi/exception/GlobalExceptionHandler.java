e05e235aeb754f134ed57b8c693ef62a5a349ea3
trbaxter <trb7074@gmail.com>
2024-06-30 11:47:22 -0500

    add'n: exception for bad json

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,6 +28,14 @@ public class GlobalExceptionHandler {
 
     return new ResponseEntity<>(
         new Result("Validation Error: " + errorMessage), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<Result> handleHttpMessageNotReadableException(
+      HttpMessageNotReadableException ex) {
+    String errorMessage = "Malformed JSON request body";
+    logger.warn("Malformed JSON request body: {}", ex.getMessage());
+    return new ResponseEntity<>(new Result("Bad Request: " + errorMessage), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(Exception.class)
