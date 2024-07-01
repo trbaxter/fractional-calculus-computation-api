e545f0385d0a1a586e7ab73630f0d875db3cd9a7
trbaxter <trb7074@gmail.com>
2024-07-01 13:16:52 -0500

    remove public access modifier

--- src/test/java/com/trbaxter/github/fractionalcomputationapi/service/integration/IntegralFormattingServiceTest.java
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/service/integration/IntegralFormattingServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class IntegralFormattingServiceTest {
+class IntegralFormattingServiceTest {
 
   @Test
   void testGetZeroPolynomialResult() {
