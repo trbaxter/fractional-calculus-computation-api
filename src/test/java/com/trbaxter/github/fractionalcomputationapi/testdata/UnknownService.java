f1f2189a3564b5940d88ae009e8b3c316baebffb
trbaxter <trb7074@gmail.com>
2024-06-30 22:04:33 -0500

    sonarcloud: remove unused parameters

--- src/test/java/com/trbaxter/github/fractionalcomputationapi/testdata/UnknownService.java
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/testdata/UnknownService.java
@@ -1,7 +1,7 @@
 package com.trbaxter.github.fractionalcomputationapi.testdata;
 
 public class UnknownService {
-  public String evaluateExpression(String polynomialExpression, double order, int precision) {
+  public String evaluateExpression() {
     return "Unknown service result";
   }
 }
