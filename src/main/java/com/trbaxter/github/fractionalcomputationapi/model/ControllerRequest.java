d39ca0082a61f945961c334b1b12fc8d564ab35d
trbaxter <trb7074@gmail.com>
2024-07-02 22:22:59 -0500

    ControllerRequest prevents NaN entry

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/model/ControllerRequest.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/model/ControllerRequest.java
@@ -1,5 +1,8 @@
 package com.trbaxter.github.fractionalcomputationapi.model;
 
+import com.trbaxter.github.fractionalcomputationapi.validation.NotNaN;
+import com.trbaxter.github.fractionalcomputationapi.validation.ValidationOrder;
+import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -12,16 +15,20 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@GroupSequence({ControllerRequest.class, ValidationOrder.First.class, ValidationOrder.Second.class})
 public class ControllerRequest {
 
-  @NotNull(message = "Polynomial expression cannot be null")
-  @NotBlank(message = "Polynomial expression cannot be blank")
+  @NotNull(message = "Polynomial expression cannot be null", groups = ValidationOrder.First.class)
+  @NotBlank(
+      message = "Polynomial expression cannot be blank",
+      groups = ValidationOrder.Second.class)
   private String polynomialExpression;
 
-  @NotNull(message = "Order cannot be null")
+  @NotNull(message = "Order cannot be null", groups = ValidationOrder.First.class)
+  @NotNaN(message = "Order cannot be NaN", groups = ValidationOrder.Second.class)
   private Double order;
 
-  @NotNull(message = "Precision cannot be null")
-  @Positive(message = "Precision must be a positive integer")
+  @NotNull(message = "Precision cannot be null", groups = ValidationOrder.First.class)
+  @Positive(message = "Precision must be a positive integer", groups = ValidationOrder.Second.class)
   private Integer precision;
 }
