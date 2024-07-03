abec4f50e3a5cd93b0dc8edecc7a32c08695a96e
trbaxter <trb7074@gmail.com>
2024-07-02 22:53:28 -0500

    removed unneeded method

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/validation/NotNaNValidator.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/validation/NotNaNValidator.java
@@ -6,9 +6,6 @@ import jakarta.validation.ConstraintValidatorContext;
 public class NotNaNValidator implements ConstraintValidator<NotNaN, Double> {
 
   @Override
-  public void initialize(NotNaN constraintAnnotation) {}
-
-  @Override
   public boolean isValid(Double value, ConstraintValidatorContext context) {
     return value != null && !value.isNaN();
   }
