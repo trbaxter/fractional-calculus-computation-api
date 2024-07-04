bcd1d0be468a6190674ca66d45ddbba214a112ad
trbaxter <trb7074@gmail.com>
2024-07-03 21:14:41 -0500

    documentation: javadoc comments

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/validation/DefaultGroupSequence.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/validation/DefaultGroupSequence.java
@@ -3,5 +3,10 @@ package com.trbaxter.github.fractionalcomputationapi.validation;
 import jakarta.validation.GroupSequence;
 import jakarta.validation.groups.Default;
 
+/**
+ * Defines the default group sequence for validation.<br>
+ * This sequence ensures that the validation groups {@link ValidationOrder.First} and {@link
+ * ValidationOrder.Second} are validated in the specified order before the {@link Default} group.
+ */
 @GroupSequence({ValidationOrder.First.class, ValidationOrder.Second.class, Default.class})
 public interface DefaultGroupSequence {}
