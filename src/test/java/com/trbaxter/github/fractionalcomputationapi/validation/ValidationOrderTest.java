e145c1d29a49c43f094e4d25141a8d638bd4e4b3
trbaxter <trb7074@gmail.com>
2024-07-03 22:17:38 -0500

    documentation: javadoc comments

---
+++ src/test/java/com/trbaxter/github/fractionalcomputationapi/validation/ValidationOrderTest.java
@@ -0,0 +1,31 @@
+package com.trbaxter.github.fractionalcomputationapi.validation;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.lang.reflect.Constructor;
+import org.junit.jupiter.api.Test;
+
+public class ValidationOrderTest {
+
+  /** Tests that the constructor of the ValidationOrder class is private and not accessible. */
+  @Test
+  void testPrivateConstructor() throws NoSuchMethodException {
+    Constructor<ValidationOrder> constructor = ValidationOrder.class.getDeclaredConstructor();
+    assertFalse(constructor.canAccess(null), "Constructor should be private and not accessible.");
+  }
+
+  /**
+   * Tests that an attempt to access the private constructor throws an InstantiationException or
+   * IllegalAccessException.
+   */
+  @Test
+  void testPrivateConstructorThrowsException() throws NoSuchMethodException {
+    Constructor<ValidationOrder> constructor = ValidationOrder.class.getDeclaredConstructor();
+    assertFalse(constructor.canAccess(null), "Constructor should be private and not accessible.");
+    assertThrows(
+        IllegalAccessException.class,
+        constructor::newInstance,
+        "Constructor should throw IllegalAccessException when accessed.");
+  }
+}
