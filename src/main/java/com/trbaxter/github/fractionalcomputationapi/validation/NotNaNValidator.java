5465a4d974c0aef861d93df5f7e80735a63a81a9
trbaxter <trb7074@gmail.com>
2024-07-03 22:07:14 -0500

    documentation: javadoc comments

--- src/main/java/com/trbaxter/github/fractionalcomputationapi/validation/NotNaNValidator.java
+++ src/main/java/com/trbaxter/github/fractionalcomputationapi/validation/NotNaNValidator.java
@@ -3,8 +3,20 @@ package com.trbaxter.github.fractionalcomputationapi.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+/**
+ * NotNaNValidator is a validator to ensure a given Double value is not NaN.
+ *
+ * <p>This validator checks that the provided value is not null and is a valid number.
+ */
 public class NotNaNValidator implements ConstraintValidator<NotNaN, Double> {
 
+  /**
+   * Validates that the given value is not NaN.
+   *
+   * @param value the value to validate
+   * @param context the context in which the constraint is evaluated
+   * @return true if the value is not null and not NaN, false otherwise
+   */
   @Override
   public boolean isValid(Double value, ConstraintValidatorContext context) {
     return value != null && !value.isNaN();
